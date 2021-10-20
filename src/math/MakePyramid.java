73f4ba2b842bb138a606fea759f04026956800fc
Asif Hossain <asif.rasbd@gmail.com>
2021-10-19 22:18:10 -0400

    MakePyramid answered

--- src/math/MakePyramid.java
+++ src/math/MakePyramid.java
@@ -1,4 +1,5 @@
 package math;
+import java.util.Scanner;
 
 public class MakePyramid {
     public static void main(String[] args) {
@@ -14,6 +15,25 @@ public class MakePyramid {
 
 
         //implementation here...
+        int i, space, rows, k=0;
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Enter number of rows :");
+        rows = scan.nextInt();
+        for(i=1; i<=rows; i++)
+        {
+            for(space=1; space<=(rows-i); space++)
+            {
+                System.out.print("");
+            }
+        while(k != (2*i-1))
+        {
+            System.out.print("*");
+            k++;
+        }
+        k = 0;
+        System.out.println();
+        }
+
 
     }
   }
